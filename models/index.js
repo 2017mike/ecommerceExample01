f6ab8cabd0dd476a91623bd006085d4011cc65dd
Michael Scharf <mwscharf@ucdavis.edu>
2022-03-15 19:49:40 -0700

    finished

--- models/index.js
+++ models/index.js
@@ -4,6 +4,29 @@ const Category = require('./Category')
 const Tag = require('./Tag')
 const ProductTag = require('./ProductTag')
 
+// * `Product` belongs to `Category`, and `Category` has many `Product` models, as a category can have multiple products but a product can only belong to one category.
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+
+// * `Product` belongs to many `Tag` models, and `Tag` belongs to many `Product` models. Allow products to have multiple tags and tags to have many products by using the `ProductTag` through model.
+
+Product.belongsToMany(Tag, {
+  through: ProductTag
+})
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
+
+
+
 // Products belongsTo Category
 
 // Categories have many Products
